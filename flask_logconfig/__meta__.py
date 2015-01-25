7cd5347fe4213c1bedb706c5978c4de56b733e94
Derrick Gilland <dgilland@gmail.com>
2015-01-25 01:08:39 -0500

    Bump dev version number.

--- flask_logconfig/__meta__.py
+++ flask_logconfig/__meta__.py
@@ -5,7 +5,7 @@ __title__ = 'Flask-LogConfig'
 __summary__ = 'Flask extension for configuring Python logging module'
 __url__ = 'https://github.com/dgilland/flask-logconfig'
 
-__version__ = '0.2.0'
+__version__ = '0.3.0-dev'
 
 __install_requires__ = ['logconfig>=0.4.0', 'flask']
 __tests_require__ = ['tox']
